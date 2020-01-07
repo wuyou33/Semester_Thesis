936e1ad21a0452bc588d9ebf0b9ecb5551629e6a
volmen3 <e.volodscoi@gmail.com>
2020-01-07 21:25:34 +0100

    pd_outer_cs adjusted + some changes in the folder structure

--- Simulink/Crazyflie_Simulation/outer_pd_cs.m
+++ Simulink/Crazyflie_Simulation/outer_pd_cs.m
@@ -32,12 +32,15 @@ function outer_pd_cs(k_dxi_in, k_xi_in, k_omega_fix, k_eta_fix)
                          (C2-2)*z^2 + (-Ts*K_omega*C1 - 2*C2 + 1)*z + C2 );
 
             TF_dxierr_dxi = K_d_xi * TF_etaref_eta * Ts*z/(z-1);     % Open loop
-            TF_dxiref_dxi = TF_dxierr_dxi / (1 + TF_dxierr_dxi);     % Closed loop
-
+            %TF_dxiref_dxi = TF_dxierr_dxi / (1 + TF_dxierr_dxi)     % Closed loop computed wrong (see "Using FEEDBACK to Close Feedback Loops" documentation)
+            TF_dxiref_dxi = feedback(TF_dxierr_dxi, 1);              % Closed loop
+            
             TF_xierr_xi = K_xi * TF_dxiref_dxi * Ts*z/(z-1);         % Open loop
-            TF_xiref_xi = TF_xierr_xi / (1 + TF_xierr_xi);           % Closed
-
+            %TF_xiref_xi = TF_xierr_xi / (1 + TF_xierr_xi);          % Closed loop computed wrong (see "Using FEEDBACK to Close Feedback Loops" documentation)
+            TF_xiref_xi = feedback(TF_xierr_xi, 1);                  % Closed loop
+                
             % Compute poles of TF_xiref_xi
+            display(['K_omega=' num2str(K_omega), ' K_eta=' num2str(K_eta)])
             display(['K_dxi=', num2str(k_dxi), ', K_xi=', num2str(k_xi)])
             p = pole(TF_xiref_xi)
 
